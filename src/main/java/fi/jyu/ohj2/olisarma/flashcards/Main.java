fcdefb750df603276307b522c3d45a44ac3ece8e
olivia kiljala <olisarma>
2026-03-17 14:43:38 +0200

    projektin alustus

---
+++ src/main/java/fi/jyu/ohj2/olisarma/flashcards/Main.java
@@ -0,0 +1,9 @@
+package fi.jyu.ohj2.olisarma.flashcards;
+
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(App.class, args);
+    }
+}
