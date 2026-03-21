dadb116b2a66ad5ad0f44678696b5720a946656a
olivia kiljala <olisarma>
2026-03-21 18:27:49 +0200

    Lisättiin Pakkaan konstruktori

--- src/main/java/model/Deck.java
+++ src/main/java/model/Deck.java
@@ -1,12 +1,32 @@
 package model;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Deck {
-    private String name; // englannin sanoja attribuutti
-    private List<Card> cards = new ArrayList<>();  // lista pakan korteista attribuutti
+    private StringProperty name; // englannin sanoja attribuutti
+    private ObservableList<Card> cards; // lista pakan korteista attribuutti
 
+    /**
+     * Konstruktori luo uuden pakan.
+     * @param name pakan nimi
+     */
+    public Deck(String name){
+        this.name = new SimpleStringProperty(name);
+        this.cards = FXCollections.observableArrayList();
+    }
+
+    /**
+     * Lisää kortin pakkaan
+     * @param card lisättävä kortti
+     */
     public void addCard(Card card){
         boolean add = cards.add(card);  //metodi , lisää kortin listaan
     }
+
+
 }
