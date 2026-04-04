6e988c80f31d5377813992877484848aad0f5f03
olivia kiljala <olisarma>
2026-04-04 21:26:16 +0300

    Uusi pakka lisäys ja takaisin nappi toimintaan ainakin

--- src/main/java/fi/jyu/ohj2/olisarma/flashcards/DeckViewController.java
+++ src/main/java/fi/jyu/ohj2/olisarma/flashcards/DeckViewController.java
@@ -1,4 +1,30 @@
 package fi.jyu.ohj2.olisarma.flashcards;
 
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+import model.Deck;
+
 public class DeckViewController {
+
+    @FXML
+    public void handleTakaisin(ActionEvent event){
+
+        // Haetaan nappia vastaava ikkuna (Stage)
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        // Suljetaan ikkuna
+        stage.close();
+    }
+
+    private Deck deck;
+
+    public void setDeck(Deck deck) {
+        this.deck = deck;
+
+        System.out.println("DeckViewController sai pakan: " + deck.getName());
+    }
+
+
 }
--- src/main/java/fi/jyu/ohj2/olisarma/flashcards/MainController.java
+++ src/main/java/fi/jyu/ohj2/olisarma/flashcards/MainController.java
@@ -5,9 +5,87 @@ import javafx.fxml.Initializable;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.ListView;
+import model.Deck;
+
+import java.io.IOException;
+
 public class MainController implements Initializable {
+
+    // Yhdistää FXML:n ListView-komponentin tähän muuttujaan pakkaListView (näkyvä käyttöliittymässä)
+    @FXML
+    private ListView<Deck> pakkaListView;
+
+    // Lista, jossa on kaikki pakat muistissa (näkyvä koodissa) decks
+    private ObservableList<Deck> decks;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        // Write initialization code here
+        decks = FXCollections.observableArrayList();  // luodaan tyhjä lista pakoille decks
+        pakkaListView.setItems(decks);  // yhdistetään listat
+    }
+
+    @FXML
+    public void handleLisaaPakka() {
+        Deck uusiPakka = new Deck("Uusi pakka");
+        decks.add(uusiPakka);
+        pakkaListView.getSelectionModel().select(uusiPakka);
+    }
+
+
+    @FXML
+    public void handlePoistaPakka() {
+        Deck valittuPakka = pakkaListView.getSelectionModel().getSelectedItem();
+
+        if (valittuPakka != null) {
+            decks.remove(valittuPakka);
+        }
+    }
+
+
+    /*
+    Avaa pakan tarkasteluun uuden ikkunan etusivu-ikkunan päälle
+     */
+    @FXML
+    public void handleAvaaPakka(ActionEvent event) {
+
+        Deck valittuPakka = pakkaListView.getSelectionModel().getSelectedItem();
+
+        if (valittuPakka == null) {
+            return;
+        }
+
+        try {
+            FXMLLoader loader = new FXMLLoader(
+                    getClass().getResource("/fi/jyu/ohj2/olisarma/flashcards/deckview.fxml")
+            );
+            Parent root = loader.load();
+
+            DeckViewController controller = loader.getController();
+            controller.setDeck(valittuPakka);
+
+            Scene scene = new Scene(root);
+
+            Stage tarkastelu = new Stage();
+            tarkastelu.setScene(scene);
+
+            tarkastelu.setTitle("Pakan tarkastelu");
+            tarkastelu.setMinWidth(500);
+            tarkastelu.setMinHeight(400);
+            tarkastelu.initModality(Modality.APPLICATION_MODAL);
+            tarkastelu.showAndWait();
+
+        }catch (IOException e) {
+                throw new RuntimeException(e);
+            }
     }
 }
